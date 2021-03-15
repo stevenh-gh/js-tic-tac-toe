c3947d3f58d2809ed3329a8b12440c9099f1e301
steven <steven.h@live.com>
2021-03-15 16:11:32 -0700

    formatting

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const board = (() => {
   let grid = [...Array(3)].map(e => Array(3))
 
-  const fill = (xo, coord) => {
-    grid[coord[0]][coord[1]] = xo
-  }
+  const fill = (xo, coord) => grid[coord[0]][coord[1]] = xo
 
   return { grid, fill }
 })()
