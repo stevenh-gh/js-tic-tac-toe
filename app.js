40266a77e0cad11ec18082346addda5110e48394
steven <steven.h@live.com>
2021-03-16 18:48:21 -0700

    add getCell method to board that gets cell number as array

--- app.js
+++ app.js
@@ -3,7 +3,18 @@ const board = (() => {
 
   const fill = (xo, coord) => grid[coord[0]][coord[1]] = xo
 
-  return { grid, fill }
+  const getCells = () => {
+    const cells = document.querySelectorAll('div[data-cell]')
+    cells.forEach(el => {
+      el.addEventListener('click', e => {
+        let cell = e.target.dataset.cell
+        let coord = [Number(cell[0]), Number(cell[1])]
+        console.log(coord)
+      })
+    })
+  }
+
+  return { grid, fill, getCells }
 })()
 
 const PlayerFactory = xo => {
